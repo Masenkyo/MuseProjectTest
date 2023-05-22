bd6277c3d51bc564b6d5a41dbf9f7d50ac2e1136
Dylan33431 <89254356+Dylan33431@users.noreply.github.com>
2023-05-22 09:45:16 +0200

    enemy

--- MuseTest/Assets/DylanYTGaming69420/scrips/ShootingEnemy.cs
+++ MuseTest/Assets/DylanYTGaming69420/scrips/ShootingEnemy.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class ShootingEnemy : MonoBehaviour
 {
-    public float speed;
-    public Transform target;
-    public float minDistance;
 
     void Update()
     {
-        if (Vector3.Distance(transform.position,target.position) > minDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        }    
+         
     }
 }
+
