902f4194867dc0d7ad7092972ad841c4ac3f44aa
Dylan33431 <89254356+Dylan33431@users.noreply.github.com>
2023-06-19 13:00:13 +0200

    powerup

--- Muse/Assets/Sil/Script/EnemyHP.cs
+++ Muse/Assets/Sil/Script/EnemyHP.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class EnemyHP : MonoBehaviour
 {
     public int HP = 10;
+    public GameObject drug;
     void Update()
     {
         if (HP <= 0)
         {
             Destroy(gameObject);
+            int randomnumber = Random.Range(1, 5); 
+            if(randomnumber == 1)
+            {
+                Instantiate(drug,gameObject.transform.position, Quaternion.identity);
+            }
         }
     }
 }
